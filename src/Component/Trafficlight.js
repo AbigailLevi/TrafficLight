1c74c2af55d0dc1c46c3ea6d26b6742c9a006257
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-13 15:27:55 -0500

    fix the sunset

---
+++ src/Component/Trafficlight.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import '../../src/App.css'
+
+function Trafficlight() {
+    return (
+        <div>
+            <div className="holder">
+            </div>
+            <div className="faller">
+                <div className="red"></div>
+                <div className="yellow"></div>
+                <div className="green"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Trafficlight
+
