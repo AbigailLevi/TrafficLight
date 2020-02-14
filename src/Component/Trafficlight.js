87eeb67731241250ef3f36394706d704c46f9dfe
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-14 10:57:24 -0500

    I finished

--- src/Component/Trafficlight.js
+++ src/Component/Trafficlight.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react'
 import '../../src/App.css'
 
 function Trafficlight() {
+    const [activateLight,setactivateLight]=useState('yellow')
     return (
         <div>
             <div className="holder">
             </div>
             <div className="faller">
-                <div className="red"></div>
-                <div className="yellow"></div>
-                <div className="green"></div>
+                <div className="red" style={activateLight==='red'?{backgroundColor:'red'}:null} onClick={()=>setactivateLight('red')}></div>
+                <div className="yellow" style={activateLight==='yellow'?{backgroundColor:'yellow'}:null} onClick={()=>setactivateLight('yellow')}></div>
+                <div className="green" style={activateLight==='green'?{backgroundColor:'limegreen'}:null} onClick={()=>setactivateLight('green')}></div>
             </div>
         </div>
     )
